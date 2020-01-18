d5347647261aabdbedd05175f966b58efa1b5f60
Anastasia <44047645+anastasiav96@users.noreply.github.com>
2020-01-18 12:59:40 +0700

    Update string_pipline.py

--- string_pipline.py
+++ string_pipline.py
@@ -2,7 +2,7 @@ def replace_points(string):
     return string.replace('.', '')
 
 
-def replace_comma(string):
+def replace_commas(string):
     return string.replace(',', '')
 
 
@@ -18,5 +18,5 @@ if __name__ == '__main__':
     file_w = open('output.txt', 'a')
     with open('data.txt', 'r') as f:
         for line in f:
-            file_w.write(lower_str(replace_double_spaces(replace_comma(replace_points(line)))))
+            file_w.write(lower_str(replace_double_spaces(replace_commas(replace_points(line)))))
     file_w.close()
