7404669891a712e8b9123330596ef7df02e3c470
Anastasia <44047645+anastasiav96@users.noreply.github.com>
2020-01-21 18:05:13 +0700

    Update needle_in_stack.py

--- needle_in_stack.py
+++ needle_in_stack.py
@@ -4,42 +4,24 @@ def measure_time(func):
     def wrapper(*args, **kwargs):
         time_start = timeit.default_timer()
         func(*args, **kwargs)
-        print(f'Function {func.__name__} takes {timeit.default_timer() - time_start} seconds')
+        print(timeit.default_timer() - time_start, 'seconds')
 
     return wrapper
 
 
 @measure_time
-def list_search(s, n):
+def search(s, n):
     for i in n:
         if i in s:
-            s.index(i)
-
-
-@measure_time
-def tuple_search(s, n):
-    for i in n:
-        if i in s:
-            s.index(i)
-
-
-@measure_time
-def dict_search(s, n):
-    for i in n:
-        if i in s.keys():
-            s.get(i)
+            pass
 
 
 def get_random_stack_needles(stack_size):
-    stack_list = [random.random() for i in range(stack_size)]
-    needles_list = [random.choice(stack_list) for j in range(stack_size // 2)]
-    needles_list.extend([random.random() for k in range(stack_size // 2)])
+    stack_list = [random.random() for _ in range(stack_size)]
+    needles_list = [random.choice(stack_list) for _ in range(500)]
+    needles_list.extend([random.random() for _ in range(500)])
 
     return stack_list, needles_list
-
-
-def get_tuple_stack(stack_list):
-    return tuple(stack_list)
 
 
 def get_dict_stack(stack_list):
@@ -49,12 +31,17 @@ def get_dict_stack(stack_list):
 if __name__ == '__main__':
     import random
 
-    size_stack = [100000, 1000000, 10000000]
+    size_stack = [1000, 10000, 100000, 1000000]
 
     for size in size_stack:
         print('Size of stack: ', size)
         stack, needles = get_random_stack_needles(size)
-
-        list_search(stack, needles)
-        tuple_search(get_tuple_stack(stack), needles)
-        dict_search(get_dict_stack(stack), needles)
+        
+        print('Search in list takes:', end=' ')
+        search(stack, needles)
+        
+        print('Search in tuple takes:', end=' ')
+        search(tuple(stack), needles)
+        
+        print('Search in dict takes:', end=' ')
+        search(get_dict_stack(stack), needles)
