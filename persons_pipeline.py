3ce35e5b3be251f88520231405d1c7e2899f8d9c
Anastasia <44047645+anastasiav96@users.noreply.github.com>
2020-01-25 12:40:29 +0700

    Update and rename persons_pipline.py to persons_pipeline.py

--- persons_pipeline.py
+++ persons_pipeline.py
@@ -3,37 +3,37 @@ def split_data(string):
 
 
 def get_id(lst):
-    match = re.search(r'\d+', lst[0])
+    match = re.search(r'^\d+$', lst[0])
     return match[0] if match else None
 
 
 def get_name(lst):
-    match = re.search(r'[a-zA-Z]+', lst[1])
+    match = re.search(r'^[a-zA-Z]+$', lst[1])
     return match[0] if match else None
 
 
 def get_age(lst):
-    match = re.search(r'\d+', lst[2])
+    match = re.search(r'^\d+$', lst[2])
     return match[0] if match else None
 
 
 def get_phone(lst):
-    match = re.search(r'^\++\d{,12}', lst[3])
+    match = re.search(r'^\+\d{,12}$', lst[3])
     return match[0] if match else None
 
 
 def get_email(lst):
-    match = re.search(r'[a-zA-Z1-9]+@+[a-zA-Z]+\.[a-zA-Z]+', lst[4])
+    match = re.search(r'^[a-zA-Z1-9]+@[a-zA-Z]+\.[a-zA-Z]+$', lst[4])
     return match[0] if match else None
 
 
 def get_role(lst):
-    match = re.search(r'\badmin\b|\buser\b|\bguest\b', lst[5])
+    match = re.search(r'^admin$|^user$|^guest$', lst[5])
     return match[0] if match else None
 
 
 def get_status(lst):
-    match = re.search(r'\bactive\b|\binactive\b', lst[6])
+    match = re.search(r'^active$|^inactive$', lst[6])
     return match[0] if match else None
 
 
@@ -47,9 +47,10 @@ if __name__ == '__main__':
 
         for line in f.readlines()[1:]:
             data = split_data(line)
-            data = [get_id(data), get_name(data), get_age(data), get_phone(data), get_email(data), get_role(data),
-                    get_status(data)]
-            person = dict(zip(keys, data))
+            val_data = [get_id(data), get_name(data), get_age(data), get_phone(data), get_email(data), get_role(data),
+                        get_status(data)]
+            
+            person = dict(zip(keys, val_data))
             persons.append(person)
             
     print(persons)
