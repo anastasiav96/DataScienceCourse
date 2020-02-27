6fb30be672d07314c8b759aa4ebd1e7c7e9bab4a
Anastasia <44047645+anastasiav96@users.noreply.github.com>
2020-02-27 21:26:12 +0700

    Create authors.py

---
+++ Task 6/authors.py
@@ -0,0 +1,39 @@
+from database import DataBase
+
+
+class Author(DataBase):
+    def __init__(self, name, surname):
+        super().__init__()
+        self.name = name
+        self.surname = surname
+
+    def add_author(self):
+        sql_statement = """INSERT INTO authors (name, surname)
+                           VALUES (%s, %s);"""
+        self.query_database(sql_statement, (self.name, self.surname,))
+        self.commit_query()
+
+    def update_author(self, id, new_name, new_surname):
+        sql_statement = """UPDATE authors SET 
+                           name = %s, surname = %s WHERE author_id = %s;"""
+        self.query_database(sql_statement, (new_name, new_surname, id))
+        self.commit_query()
+        self.name = new_name
+        self.surname = new_surname
+
+    def delete_author(self, id):
+        sql_statement = """DELETE FROM authors WHERE author_id = %s;"""
+        self.query_database(sql_statement, (id,))
+        self.commit_query()
+
+    def delete_all_authors(self):
+        sql_statement = """DELETE FROM authors;"""
+        self.query_database(sql_statement)
+        self.commit_query()
+
+    def __del__(self):
+        self.conn.close()
+        self.cursor.close()
+
+    def __str__(self):
+        return f'Author name: {self.name}, author surname: {self.surname}'
